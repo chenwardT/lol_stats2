863b4536369ce52949743c565ece913ce84f6978
Edward Chen <chenward.t@gmail.com>
2015-05-12 07:31:10 -0400

    Cleanup

--- lol_stats2/urls.py
+++ lol_stats2/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers
 
 from summoners.views import SummonerViewSet
 
