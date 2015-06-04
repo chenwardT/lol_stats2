c7d34a739f283beaf27244442e725dc3c7104f99
Edward Chen <chenward.t@gmail.com>
2015-06-04 04:21:52 -0400

    League and LeagueEntry managers. Added losses to LE model. Output formatting.

--- leagues/models.py
+++ leagues/models.py
@@ -1,5 +1,29 @@
 from django.db import models
 
+# TODO: Cleanup.
+class LeagueManager(models.Manager):
+    def create_or_update_league(self, attrs, region):
+        possibly_extant_league = self.filter(region=region,
+                                             queue=attrs['queue'],
+                                             name=attrs['name'],
+                                             tier=attrs['tier'])
+        if possibly_extant_league.exists():
+            league = possibly_extant_league[0]
+            self.update_league(league, attrs, region)
+        else:
+            self.create_league(attrs, region)
+
+    # TODO: Factor out creation of entries.
+    def create_league(self, attrs, region):
+        league = self.create(region=region, queue=attrs['queue'],
+                             name=attrs['name'], tier=attrs['tier'])
+        for entry in attrs['entries']:
+            league.leagueentry_set.create_entry(entry)
+
+    def update_league(self, league, attrs, region):
+        for entry in attrs['entries']:
+            league.leagueentry_set.create_entry(entry)
+
 class League(models.Model):
     """
     Maps to Riot API League DTO.
@@ -10,13 +34,27 @@ class League(models.Model):
     name = models.CharField(max_length=32)      # ex. Orianna's Warlocks
     tier = models.CharField(max_length=12)      # ex. CHALLENGER
 
+    objects = LeagueManager()
+
     def __str__(self):
-        return '{} {} {} {}'.format(self.region, self.queue, self.name,
-                                    self.tier)
+        return '[{}] {} {} {}'.format(self.region, self.queue, self.name,
+                                      self.tier)
 
     class Meta:
         unique_together = ('region', 'queue', 'name', 'tier')
 
+class LeagueEntryManager(models.Manager):
+    def create_entry(self, attrs):
+        entry = self.create(division=attrs['division'],
+                            is_fresh_blood=attrs['isFreshBlood'],
+                            is_hot_streak=attrs['isHotStreak'],
+                            is_inactive=attrs['isInactive'],
+                            is_veteran=attrs['isVeteran'],
+                            league_points=attrs['leaguePoints'],
+                            player_or_team_id=attrs['playerOrTeamId'],
+                            player_or_team_name=attrs['playerOrTeamName'],
+                            wins=attrs['wins'],
+                            losses=attrs['losses'])
 
 class LeagueEntry(models.Model):
     """
@@ -37,6 +75,7 @@ class LeagueEntry(models.Model):
     player_or_team_id = models.CharField(max_length=64)     # ex. TEAM-68594bb0-cce0-11e3-a7cc-782bcb4d1861
     player_or_team_name = models.CharField(max_length=24)   # ex. Smiteless Baron
     wins = models.IntegerField()
+    losses = models.IntegerField()
 
     # MiniSeries DTO
     series_losses = models.SmallIntegerField(null=True, blank=True)
@@ -46,8 +85,10 @@ class LeagueEntry(models.Model):
 
     league = models.ForeignKey(League)
 
+    objects = LeagueEntryManager()
+
     def __str__(self):
-        return '{} {} - {} ({})'.format(self.league,
+        return '<{}> {}: {} ({})'.format(self.league,
                                         self.division,
                                         self.player_or_team_name,
                                         self.league_points)
