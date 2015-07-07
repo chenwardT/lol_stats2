e469536e5712933e43960c8e58f612fbb00f4b23
Edward Chen <chenward.T@gmail.com>
2015-07-06 20:07:00 -0400

    LeagueAdmin with LeagueEntryInline, sensible filters and columns.

--- leagues/admin.py
+++ leagues/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import League, LeagueEntry
+
+class LeagueEntryInline(admin.TabularInline):
+    model = LeagueEntry
+
+@admin.register(League)
+class LeagueAdmin(admin.ModelAdmin):
+    list_display = ('name', 'tier', 'queue', 'region')
+
+    inlines = [
+        LeagueEntryInline,
+    ]
+
+    list_filter = ('region', 'queue', 'tier')
