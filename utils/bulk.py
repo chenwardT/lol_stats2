07bcf755c3c008a373f1e05ee30a6da50feca68f
Edward Chen <chenward.T@gmail.com>
2015-07-08 12:38:10 -0400

    utils.bulk: get_summoners_from_league_entries to mass query + store summoners from league entries.

--- utils/bulk.py
+++ utils/bulk.py
@@ -1,5 +1,6 @@
 from .functions import chunks
 from leagues.models import LeagueEntry
+from summoners.models import Summoner
 from riot_api.wrapper import RiotAPI
 
 def get_league_if_none(summoners=None):
@@ -35,3 +36,43 @@ def get_league_if_none(summoners=None):
 
     for group in chunked:
         RiotAPI.get_leagues(group, region)
+
+def get_summoners_from_league_entries(league_entries=None, region=None):
+    """
+    Queries for chunks of summoners whose IDs are known via LeagueEntry records
+    but are not stored in the Summoner table.
+
+    Accepts an iterable of LeagueEntry objects and a region.
+    Summoner queries are sent as chunks of at most 40 IDs.
+    """
+    _MAX_SUMMONER_IDS_PER_QUERY = 40
+
+    if league_entries is None:
+        raise ValueError('league_entries must not be None.')
+
+    # TODO: Validate input.
+    if region is None:
+        raise ValueError('region must not be None.')
+
+    to_query = set()
+
+    for le in league_entries:
+        if not Summoner.objects.filter(region=le.league.region,
+                                       summoner_id=le.player_or_team_id).exists():
+            to_query.add(le.player_or_team_id)
+
+    query_list = []
+
+    for player in to_query:
+        query_list.append(player)
+
+    chunked = []
+
+    for chunk in chunks(query_list, _MAX_SUMMONER_IDS_PER_QUERY):
+        chunked.append(chunk)
+
+    print("{} queries will be made to fetch {} summoners.".format(len(chunked),
+                                                                  len(query_list)))
+
+    for group in chunked:
+        RiotAPI.get_summoners(ids=group, region=region)
