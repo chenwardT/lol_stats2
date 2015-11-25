7a5760ed5e716fb7b1195fa78f7cac652e899ba7
Edward Chen <chenward.T@gmail.com>
2015-11-25 17:10:38 -0500

    Add region selection field to summoner search form.

--- summoners/forms.py
+++ summoners/forms.py
@@ -4,3 +4,15 @@ from django import forms
 class SearchForm(forms.Form):
     # region = forms.CharField(label='Region')
     name = forms.CharField(label='Summoner', max_length=24)
+    region = forms.ChoiceField(label='Region', choices=(
+        ('BR', 'BR'),
+        ('EUNE', 'EUNE'),
+        ('EUW', 'EUW'),
+        ('KR', 'KR'),
+        ('LAN', 'LAN'),
+        ('LAS', 'LAS'),
+        ('NA', 'NA'),
+        ('OCE', 'OCE'),
+        ('RU', 'RU'),
+        ('TR', 'TR'),
+    ))
