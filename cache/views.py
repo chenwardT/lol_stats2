05e5635528f7f7cba49c71c41c4ac7410133a4c5
Edward Chen <chenward.T@gmail.com>
2015-11-25 17:07:04 -0500

    Add is_summoner_refreshable ajax view to cache/views. Use HttpResponseBadRequest on unexpected requests in cache/views. Cleanup cache/views.

--- cache/views.py
+++ cache/views.py
@@ -1,60 +1,34 @@
 import logging
 
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse, HttpResponseBadRequest
 
 # from .summoners import SingleSummoner
-# from summoners.models import standardize_name
+from summoners.models import Summoner
 from utils.functions import multi_task_status
 
 logger = logging.getLogger(__name__)
 
 
-# TODO: Remove CSRF exemption.
-
-# @csrf_exempt
-# def ajax_summoner_query(request):
-#     # logger.debug(request.META)
-#     if request.is_ajax():
-#         region = request.POST['region'].upper()
-#         name = standardize_name(request.POST['name'])
-#         logger.debug('region: {}, name: {}'.format(region, name))
-#         ss = SingleSummoner(std_name=name, region=region)
-#         ss.full_query()
-#
-#         return JsonResponse({'region': region, 'name': name})
-#     else:
-#         return JsonResponse({'error': 'error'})
-#
-#
-# @csrf_exempt
-# def ajax_is_eligible_for_refresh(request):
-#     logger.debug(request.POST)
-#
-#     if request.is_ajax():
-#         region = request.POST['region'].upper()
-#         id = request.POST['id']
-#
-#         logger.debug('region: {}, ID: {}'.format(region, id))
-#         ss = SingleSummoner(summoner_id=id, region=region)
-#         result = ss.is_cache_fresh()
-#         logger.debug('summoner [{}] {} eligible for refresh: {}'
-#                      .format(region, id, result))
-#
-#         return JsonResponse({'result': result})
-#     else:
-#         return JsonResponse({'error': 'error'})
-
-
-@csrf_exempt
 def task_status(request):
+    # logger.debug(request.POST)
     logger.debug(request.POST.getlist('task_ids[]'))
 
+    # Append [] to refer to a list sent by jQuery.
     if request.is_ajax():
-        # Append [] to refer to a list sent by jQuery.
         if 'task_ids[]' in request.POST:
             task_ids = request.POST.getlist('task_ids[]')
-
             return JsonResponse({'success': multi_task_status(task_ids)})
+        else:
+            return HttpResponseBadRequest
+    else:
+        return HttpResponseBadRequest
 
-    return JsonResponse({'error': 'unrecognized request'})
+
+def is_summoner_refreshable(request):
+    logger.debug(request.POST)
+
+    if request.is_ajax() and 'pk' in request.POST:
+        pk = request.POST['pk']
+        return JsonResponse({'refreshable': Summoner.objects.get(pk=pk).is_refreshable()})
+    else:
+        return HttpResponseBadRequest
