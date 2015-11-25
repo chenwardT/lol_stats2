75284f8fa63f3da11e69abb50add586ddf93f28b
Edward Chen <chenward.T@gmail.com>
2015-11-25 17:08:12 -0500

    Route for is_summoner_refreshable. Rename task_status to be hyphenated. Add region regex capture to /show route.

--- lol_stats2/urls.py
+++ lol_stats2/urls.py
@@ -6,7 +6,7 @@ from summoners.views import SummonerViewSet, index, search, show, refresh
 from champions.views import ChampionViewSet
 from matches.views import MatchDetailViewSet, MatchListForSummoner
 from leagues.views import LeagueViewSet, LeagueEntryForSummoner
-from cache.views import task_status
+from cache.views import task_status, is_summoner_refreshable
 
 router = routers.DefaultRouter()
 router.register(r'summoners', SummonerViewSet)
@@ -24,10 +24,11 @@ urlpatterns = patterns('',
     url(r'^summoner-league/(?P<summoner_pk>\d+)', LeagueEntryForSummoner.as_view()),
 
     url(r'^home/', index),
-    url(r'^show/(?P<name>\w+)', show, name='show'),
+    url(r'^show/(?P<region>\w+)/(?P<name>[\w ]+)', show, name='show'),
     url(r'^search/', search),
     url(r'^refresh/', refresh, name='refresh'),
-    url(r'^task_status/', task_status, name='task_status'),
+    url(r'^task-status/', task_status, name='task_status'),
+    url(r'^summoner-refreshable', is_summoner_refreshable, name='summoner_refreshable'),
 
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
