b1ca9f6d527298e837dfbd1bacab15aacc18cbbb
Edward Chen <chenward.T@gmail.com>
2015-12-03 09:11:48 -0500

    Use matches(0) instead of default 10 match limit on summoner-matches queryset.

--- matches/views.py
+++ matches/views.py
@@ -27,15 +27,13 @@ class MatchListForSummoner(generics.ListAPIView):
     This view should return a list of all the matches for
     the summoner as determined by the summoner_pk portion
     of the URL.
-
-    It returns matches in sets of at most 10.
     """
     serializer_class = MatchDetailSerializer
     pagination_class = MatchDetailResultsSetPagination
 
     def get_queryset(self):
         summoner_pk = self.kwargs['summoner_pk']
-        queryset = Summoner.objects.get(pk=summoner_pk).matches(10)
+        queryset = Summoner.objects.get(pk=summoner_pk).matches(0)
         queryset = self.get_serializer_class().setup_eager_loading(queryset)
 
         return queryset
