8f50ded73195c9c88ece29452d08d14b141d7272
Edward Chen <chenward.T@gmail.com>
2016-01-15 04:55:17 -0500

    Add match_count, win_count, win_rate instance methods to Champion model.

--- champions/models.py
+++ champions/models.py
@@ -1,6 +1,7 @@
 import logging
 
 from django.db import models
+from django.apps import apps
 
 logger = logging.getLogger(__name__)
 
@@ -31,3 +32,64 @@ class Champion(models.Model):
 
     def __str__(self):
         return self.name
+
+    # TODO: Cache args and result.
+    def match_count(self, version=None, region='NA'):
+        """
+        Returns the number of matches that this champion has participated in.
+
+        Accepts parameters for game version and region to consider.
+        If `version` is omitted, it will default to all versions.
+        If `region` is omitted, it will default to 'NA'.
+
+        Note that this logic only applies to ranked matches, where only one team
+        may have any given champion.
+        """
+        participant_model = apps.get_model('matches', 'Participant')
+        match_detail_model = apps.get_model('matches', 'MatchDetail')
+
+        # FIXME: Hack - in prod we would want a better way of getting latest version.
+        if version == 'LATEST':
+            version = match_detail_model.objects.order_by('match_creation').last().match_version
+
+        queryset = participant_model.objects.select_related('match_detail') \
+            .filter(match_detail__region=region) \
+            .filter(champion_id=self.champion_id)
+
+        if version:
+            queryset = queryset.filter(match_detail__match_version=version)
+
+        return queryset.count()
+
+    # TODO: Cache args and result.
+    def win_count(self, version=None, region='NA'):
+        """
+        Returns the number of matches that this champion has participated in and won.
+
+        Accepts parameters for game version and region to consider.
+        If `version` is omitted, it will consider all versions.
+        If `region` is omitted, it will default to 'NA'.
+
+        Note that this logic only applies to ranked matches, where only one team
+        may have any given champion.
+        """
+        participant_model = apps.get_model('matches', 'Participant')
+        match_detail_model = apps.get_model('matches', 'MatchDetail')
+
+        # FIXME: Hack - in prod we would want a better way of getting latest version.
+        if version == 'LATEST':
+            version = match_detail_model.objects.order_by('match_creation').last().match_version
+
+        queryset = participant_model.objects.select_related('match_detail') \
+            .filter(match_detail__region=region) \
+            .filter(champion_id=self.champion_id) \
+            .filter(winner=True)
+
+        if version:
+            queryset = queryset.filter(match_detail__match_version=version)
+
+        return queryset.count()
+
+    # TODO: Cache args and result.
+    def win_rate(self, version=None, region='NA'):
+        return self.win_count(version, region) / self.match_count(version, region)
