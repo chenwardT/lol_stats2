9abc03732c4b3c78b4e518a39606dcf9cc9432b3
Edward Chen <chenward.T@gmail.com>
2016-01-16 05:04:29 -0500

    Allow partial version filtering in Champion methods: total_picks, total_wins.

--- champions/models.py
+++ champions/models.py
@@ -4,10 +4,12 @@ from django.db import models
 from django.apps import apps
 
 from stats.models import ChampionStats
+from utils.functions import is_complete_version
 
 logger = logging.getLogger(__name__)
 
 class ChampionManager(models.Manager):
+    # TODO: Replaceable by Champion.objects.create()
     def create_champion(self, attrs):
         """
         Create a Champion from a dict.
@@ -42,19 +44,28 @@ class Champion(models.Model):
         Returns the number of matches that this champion has participated in.
 
         Accepts parameters for lane, role, game version and region to consider.
+
         Valid options for `lane` are 'TOP', 'MIDDLE', 'JUNGLE', 'BOTTOM' or 'ALL'
         to consider all lanes.
+
         Valid options for `role` are 'SOLO', 'NONE', 'DUO', 'DUO_CARRY', 'DUO_SUPPORT'
         or 'ALL' to consider all roles.
 
-        If `version` is omitted, it will default to all versions.
-        If `region` is omitted, it will default to 'NA'.
+        Valid options for `version` are:
+            -Complete version string, e.g. '5.21.0.413'.
+            -Incomplete version string, e.g. '5.21'. This will match the most significant
+             version numbers. Any string containing less than 4 groups of integers will
+             be considered incomplete.
+            -'ALL'. This will match all versions.
 
-        Note that this logic only applies to ranked matches, where only one team
+        If `region` is omitted, it will default to 'NA'. See utils.constants.
+
+        Note that the summing logic only applies to ranked matches, where only one team
         may have any given champion.
         """
         participant_model = apps.get_model('matches', 'Participant')
         match_detail_model = apps.get_model('matches', 'MatchDetail')
+        complete_version = False
 
         # FIXME: Hack - in prod we would want a better way of getting latest version.
         if version == 'LATEST':
@@ -73,11 +84,16 @@ class Champion(models.Model):
                 .filter(participanttimeline__role=role)
 
         if version and version is not 'ALL':
-            queryset = queryset.filter(match_detail__match_version=version)
+            if is_complete_version(version):
+                queryset = queryset.filter(match_detail__match_version=version)
+                complete_version = True
+            else:
+                queryset = queryset.filter(match_detail__match_version__startswith=version)
 
         result = queryset.count()
 
         ChampionStats.objects.upsert(lane=lane, role=role, version=version,
+                                     is_exact_version=complete_version,
                                      region=region, champion_id=self.champion_id,
                                      update_fields={'total_picks': result})
 
@@ -88,14 +104,28 @@ class Champion(models.Model):
         Returns the number of matches that this champion has participated in and won.
 
         Accepts parameters for lane, role, game version and region to consider.
-        If `version` is omitted, it will consider all versions.
-        If `region` is omitted, it will default to 'NA'.
 
-        Note that this logic only applies to ranked matches, where only one team
+        Valid options for `lane` are 'TOP', 'MIDDLE', 'JUNGLE', 'BOTTOM' or 'ALL'
+        to consider all lanes.
+
+        Valid options for `role` are 'SOLO', 'NONE', 'DUO', 'DUO_CARRY', 'DUO_SUPPORT'
+        or 'ALL' to consider all roles.
+
+        Valid options for `version` are:
+            -Complete version string, e.g. '5.21.0.413'.
+            -Incomplete version string, e.g. '5.21'. This will match the most significant
+             version numbers. Any string containing less than 4 groups of integers will
+             be considered incomplete.
+            -'ALL'. This will match all versions.
+
+        If `region` is omitted, it will default to 'NA'. See utils.constants.
+
+        Note that the summing logic only applies to ranked matches, where only one team
         may have any given champion.
         """
         participant_model = apps.get_model('matches', 'Participant')
         match_detail_model = apps.get_model('matches', 'MatchDetail')
+        complete_version = False
 
         # FIXME: Hack - in prod we would want a better way of getting latest version.
         if version == 'LATEST':
@@ -115,11 +145,16 @@ class Champion(models.Model):
                 .filter(participanttimeline__role=role)
 
         if version and version is not 'ALL':
-            queryset = queryset.filter(match_detail__match_version=version)
+            if is_complete_version(version):
+                queryset = queryset.filter(match_detail__match_version=version)
+                complete_version = True
+            else:
+                queryset = queryset.filter(match_detail__match_version__startswith=version)
 
         result = queryset.count()
 
         ChampionStats.objects.upsert(lane=lane, role=role, version=version,
+                                     is_exact_version=complete_version,
                                      region=region, champion_id=self.champion_id,
                                      update_fields={'total_wins': result})
 
