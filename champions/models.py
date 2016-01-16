289e221b75e15acd50a8a985b4475205fbce98eb
Edward Chen <chenward.T@gmail.com>
2016-01-16 03:14:24 -0500

    Refactor Champion model methods for total wins and total picks. Now allows 'ALL' for lane, role, version and region args to prevent filtering on them. Results are stored using the new ChampionStatsManager upsert method.

--- champions/models.py
+++ champions/models.py
@@ -3,6 +3,8 @@ import logging
 from django.db import models
 from django.apps import apps
 
+from stats.models import ChampionStats
+
 logger = logging.getLogger(__name__)
 
 class ChampionManager(models.Manager):
@@ -22,6 +24,8 @@ class ChampionManager(models.Manager):
 class Champion(models.Model):
     """
     Maps to Riot API champion DTO.
+
+    Contains methods to determine and store summation-related statistics.
     """
     champion_id = models.IntegerField(primary_key=True, db_index=True)
     title = models.CharField(max_length=32)
@@ -33,12 +37,16 @@ class Champion(models.Model):
     def __str__(self):
         return self.name
 
-    # TODO: Cache args and result.
-    def match_count(self, version=None, region='NA'):
+    def total_picks(self, lane='ALL', role='ALL', version='ALL', region='NA'):
         """
         Returns the number of matches that this champion has participated in.
 
-        Accepts parameters for game version and region to consider.
+        Accepts parameters for lane, role, game version and region to consider.
+        Valid options for `lane` are 'TOP', 'MIDDLE', 'JUNGLE', 'BOTTOM' or 'ALL'
+        to consider all lanes.
+        Valid options for `role` are 'SOLO', 'NONE', 'DUO', 'DUO_CARRY', 'DUO_SUPPORT'
+        or 'ALL' to consider all roles.
+
         If `version` is omitted, it will default to all versions.
         If `region` is omitted, it will default to 'NA'.
 
@@ -56,17 +64,30 @@ class Champion(models.Model):
             .filter(match_detail__region=region) \
             .filter(champion_id=self.champion_id)
 
-        if version:
+        if lane and lane is not 'ALL':
+            queryset = queryset.prefetch_related('participanttimeline_set') \
+                .filter(participanttimeline__lane=lane)
+
+        if role and role is not 'ALL':
+            queryset = queryset.prefetch_related('participanttimeline_set') \
+                .filter(participanttimeline__role=role)
+
+        if version and version is not 'ALL':
             queryset = queryset.filter(match_detail__match_version=version)
 
-        return queryset.count()
+        result = queryset.count()
 
-    # TODO: Cache args and result.
-    def win_count(self, version=None, region='NA'):
+        ChampionStats.objects.upsert(lane=lane, role=role, version=version,
+                                     region=region, champion_id=self.champion_id,
+                                     update_fields={'pick_count': result})
+
+        return result
+
+    def total_wins(self, lane='ALL', role='ALL', version='ALL', region='NA'):
         """
         Returns the number of matches that this champion has participated in and won.
 
-        Accepts parameters for game version and region to consider.
+        Accepts parameters for lane, role, game version and region to consider.
         If `version` is omitted, it will consider all versions.
         If `region` is omitted, it will default to 'NA'.
 
@@ -85,11 +106,29 @@ class Champion(models.Model):
             .filter(champion_id=self.champion_id) \
             .filter(winner=True)
 
-        if version:
+        if lane and lane is not 'ALL':
+            queryset = queryset.prefetch_related('participanttimeline_set') \
+                .filter(participanttimeline__lane=lane)
+
+        if role and role is not 'ALL':
+            queryset = queryset.prefetch_related('participanttimeline_set') \
+                .filter(participanttimeline__role=role)
+
+        if version and version is not 'ALL':
             queryset = queryset.filter(match_detail__match_version=version)
 
-        return queryset.count()
+        result = queryset.count()
 
-    # TODO: Cache args and result.
-    def win_rate(self, version=None, region='NA'):
-        return self.win_count(version, region) / self.match_count(version, region)
+        ChampionStats.objects.upsert(lane=lane, role=role, version=version,
+                                     region=region, champion_id=self.champion_id,
+                                     update_fields={'win_count': result})
+
+        return result
+
+    # TODO: Unused for now - consider moving calculations to separate module.
+    def win_rate(self, lane, role, version=None, region='NA'):
+        try:
+            return self.total_wins(lane, role, version, region) / self.total_picks(lane, role, version, region) * 100
+        except ZeroDivisionError:
+            logger.exception('ZeroDivisionError: are there 0 matches?')
+            return -1
