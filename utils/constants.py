41f74fe40379735a5a9c347c89ea1dd25f1b948d
Edward Chen <chenward.T@gmail.com>
2016-01-16 04:47:15 -0500

    Add utils.constants module.

---
+++ utils/constants.py
@@ -0,0 +1,34 @@
+VALID_LANE_ROLE_COMBOS = [
+    {'lane': 'TOP',    'role': 'DUO'},
+    {'lane': 'TOP',    'role': 'DUO_CARRY'},
+    {'lane': 'TOP',    'role': 'DUO_SUPPORT'},
+    {'lane': 'TOP',    'role': 'NONE'},
+    {'lane': 'TOP',    'role': 'SOLO'},
+
+    {'lane': 'JUNGLE', 'role': 'NONE'},
+
+    {'lane': 'MIDDLE', 'role': 'DUO'},
+    {'lane': 'MIDDLE', 'role': 'DUO_CARRY'},
+    {'lane': 'MIDDLE', 'role': 'DUO_SUPPORT'},
+    {'lane': 'MIDDLE', 'role': 'NONE'},
+    {'lane': 'MIDDLE', 'role': 'SOLO'},
+
+    {'lane': 'BOTTOM', 'role': 'DUO'},
+    {'lane': 'BOTTOM', 'role': 'DUO_CARRY'},
+    {'lane': 'BOTTOM', 'role': 'DUO_SUPPORT'},
+    {'lane': 'BOTTOM', 'role': 'NONE'},
+    {'lane': 'BOTTOM', 'role': 'SOLO'},
+]
+
+REGIONS = [
+    'BR',   # Brazil
+    'EUNE', # Europe Northeast
+    'EUW',  # Europe West
+    'KR',   # Korea
+    'LAN',  # Latin America North
+    'LAS',  # Latin America South
+    'NA',   # North America
+    'OCE',  # Oceania
+    'RU',   # Russia
+    'TR',   # Turkey
+]
