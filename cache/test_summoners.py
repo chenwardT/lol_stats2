99c307b8ff6353c8b92d3dab35ee173d9c675f9c
Edward Chen <chenward.T@gmail.com>
2016-01-19 01:35:26 -0500

    Add some SingleSummoner tests.

--- cache/test_summoners.py
+++ cache/test_summoners.py
@@ -3,7 +3,10 @@ from django.test import TestCase
 from summoners.models import Summoner
 from .summoners import SingleSummoner
 
-
+# FIXME: As it stands, this exceeds the rate limit and starts failing for reasons related.
+# Possible fixes:
+#   - emulate rate limiting
+#   - mock calls to Riot's API
 class SingleSummonerTestCase(TestCase):
     def setUp(self):
         self.known_summoner = Summoner.objects.create(summoner_id=1, name='Laughing Stapler',
@@ -12,14 +15,50 @@ class SingleSummonerTestCase(TestCase):
         self.summoner_name = 'Laughing Stapler'
         self.region = 'NA'
 
+    # Is this even necessary? If so, need to also delete everything that SingleSummoner
+    # queries create, like matches, leagues, etc.
+    # def tearDown(self):
+    #     Summoner.objects.all().delete()
+
     def test_init_by_name(self):
-        known_ss = SingleSummoner(name=self.summoner_name, region=self.region)
+        ss = SingleSummoner(name='Laughing Stapler', region='NA')
+        self.assertEqual(ss.std_name, 'laughingstapler')
+        self.assertEqual(ss.region, 'NA')
 
-        self.assertEqual(known_ss.std_name, 'laughingstapler')
-        self.assertEqual(known_ss.region, 'NA')
+    def test_init_by_id(self):
+        ss = SingleSummoner(summoner_id=123456, region='NA')
+        self.assertEqual(ss.summoner_id, 123456)
+        self.assertEqual(ss.region, 'NA')
 
-        # TODO: Rewrite init to not wait on RiotAPI.get_sumoners in the event
-        # that the summoner is not already known.
-        # Test expectations of rewrite.
+    def test_is_known(self):
+        ss = SingleSummoner(name=self.summoner_name, region=self.region)
+        self.assertTrue(ss.is_known())
+
+        ss = SingleSummoner(name='RiotNonextantName', region='NA')
+        self.assertFalse(ss.is_known())
+
+    def test_first_time_query_of_extant_summoner(self):
+        name = 'Ronfar'
+        region = 'NA'
+        ss = SingleSummoner(name=name, region=region)
+        self.assertFalse(ss.is_known())
+        result = ss.first_time_query()
+
+        self.assertTrue(result)
+        self.assertTrue(Summoner.objects.filter(region=region).filter(name__iexact=name).exists())
+        self.assertEqual(Summoner.objects.filter(region=region).filter(name__iexact=name).count(), 1)
+
+    def test_first_time_query_of_nonextant_summoner(self):
+        """
+        Ensure the summoner query fails and the query is added to the InvalidSummonerQuery table.
+        """
+        name = 'RiotNonextantName'
+        region = 'NA'
+        ss = SingleSummoner(name=name, region=region)
+        result = ss.first_time_query()
+
+        self.assertFalse(result)
+        self.assertFalse(Summoner.objects.filter(region=region).filter(name__iexact=name).exists())
+        self.assertTrue(ss.is_invalid_query())
 
 
