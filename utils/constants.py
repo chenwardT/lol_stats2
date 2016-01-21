b113b2d9cc5eefc1801b4c000b34819e74b1a086
Edward Chen <chenward.T@gmail.com>
2016-01-21 13:25:32 -0500

    Add TIER_ENUM to utils.constants.

--- utils/constants.py
+++ utils/constants.py
@@ -32,3 +32,14 @@ REGIONS = [
     'RU',   # Russia
     'TR',   # Turkey
 ]
+
+TIER_ENUM = {
+    'UNRANKED':     0,
+    'BRONZE':       1,
+    'SILVER':       2,
+    'GOLD':         3,
+    'PLATINUM':     4,
+    'DIAMOND':      5,
+    'MASTER':       6,
+    'CHALLENGER':   7,
+}
