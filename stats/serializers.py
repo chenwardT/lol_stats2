1b75b25bad7c31f6f77820d6633682972695d8ad
Edward Chen <chenward.T@gmail.com>
2016-01-25 20:40:21 -0500

    Flatten output of ChampionStats serializer. Use lane-role alias if exists.

--- stats/serializers.py
+++ stats/serializers.py
@@ -1,17 +1,33 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
-from champions.serializers import ChampionSerializer
+from utils.constants import VALID_LANE_ROLE_COMBOS
 from champions.models import Champion
-from .models import ChampionStats, Bucket
+from .models import ChampionStats
 
-class BucketSerializer(ModelSerializer):
-    class Meta:
-        model = Bucket
 
 class ChampionStatsSerializer(ModelSerializer):
-    bucket = BucketSerializer(read_only=True)
-    champion = ChampionSerializer(read_only=True)
+    name = SerializerMethodField()
+    champion_id = SerializerMethodField()
+    role = SerializerMethodField()
+
+    def get_name(self, champion_stats):
+        return champion_stats.champion.name
+
+    def get_champion_id(self, champion_stats):
+        return champion_stats.champion.champion_id
+
+    def get_role(self, champion_stats):
+        lane = champion_stats.bucket.lane
+        role = champion_stats.bucket.role
+
+        for combo in VALID_LANE_ROLE_COMBOS:
+            if combo['lane'] == lane and combo['role'] == role:
+                if 'alias' in combo.keys():
+                    return combo['alias']
+                else:
+                    return '{} {}'.format(combo['lane'], combo['role'])
 
     class Meta:
         model = ChampionStats
-        fields = ['avg_{}'.format(f) for f in Champion.aggregable_participant_fields] + ['champion', 'bucket']
+        fields = ['avg_{}'.format(f) for f in Champion.aggregable_participant_fields] + \
+                 ['name', 'champion_id', 'role', 'id']
